770a3ee7ee8452cf89f9248828eedd150bcae40b
Marcos Vinicius Pinto Fonseca <marcos.fonseca@rededor.com.br>
2019-05-21 14:30:03 -0300

    Front-end e refinamento css para menu

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,10 @@ import { createAppContainer, createDrawerNavigator } from "react-navigation";
 import Sidebar from "./pages/sidebar"
 
 import HospitalsScreen from "./pages/hospitals";
+
 import SignInScreen from './pages/signIn';
-import SettingsScreen from "./pages/settings"
-import CommentsScreen from "./pages/comments"
 
 import PatientsScreen from "./pages/patients";
-import PatientScreen from './pages/patients/patient';
 
 import PatientDetailScreen from './pages/patients/patientDetail';
 
@@ -19,23 +17,14 @@ const AppNavigator = createDrawerNavigator({
 	SignIn: {
 		screen: SignInScreen
 	},
-	Patient: {
-		screen: PatientScreen
-	},
-	PatientDetail: {
-		screen: PatientDetailScreen
-	},
 	Hospitals: {
 		screen: HospitalsScreen
 	},
 	Patients: {
 		screen: PatientsScreen
 	},
-	Comments: {
-		screen: CommentsScreen
-	},
-	Settings: {
-		screen: SettingsScreen
+	PatientDetail: {
+		screen: PatientDetailScreen
 	},
 },
 {
