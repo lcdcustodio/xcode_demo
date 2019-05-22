d6cbcdd4cb0fb396c67d9b4bf0b6427ec9e24e2d
jmjesus <jean.jesus@rededor.com.br>
2019-05-22 08:39:29 -0300

    [FIX] MC-34 Refinamento tela de pacientes, adicionado Seta de retorno, nome dos hospital no cabeçalho e adicionado icone de situacao do paciente

--- src/pages/hospitals/index.js
+++ src/pages/hospitals/index.js
@@ -39,7 +39,6 @@ export default class Hospital extends Component {
 	renderItem = ({ item }) => (
 		<TouchableOpacity
 			onPress={() => {
-				console.log(item);
 				this.props.navigation.navigate("Patients", { hospital: item });
 			}}>
 			
@@ -48,8 +47,8 @@ export default class Hospital extends Component {
 					<Thumbnail square large source={{uri: item.image}} style={styles.hospitalIcon} />
 				</View>
 				<View style={{width: 20, marginRight: 100, marginBottom: 10}}>
-					<View style={{wordWrap: 'break-word'}}>
-						<Text style={[styles.title, styles.niceBlue, {width: 300}]}> 
+					<View>
+						<Text style={[styles.title, styles.niceBlue]}> 
 							{item.title} | 
 							<Text style={[styles.description, styles.niceBlue]}> {item.date}</Text>
 						</Text>
---
+++ src/pages/patients/style.js
@@ -0,0 +1,70 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+	
+	productContainer: {
+		backgroundColor: "#FFF",
+		borderBottomWidth: 1,
+		borderBottomColor: "#DDD",
+        padding: 20, 
+        justifyContent: "space-between", 
+        flexDirection: "row", 
+        alignItems: 'center'
+	},
+
+	patientTitle: {
+		width: 171,
+        height: 14,
+        fontFamily: "Gotham Rounded",
+        fontSize: 14,
+        fontWeight: "500",
+        fontStyle: "normal",
+        lineHeight: 17,
+        letterSpacing: 0,
+        textAlign: "left"
+	},
+
+	productDescription: {
+        width: 171,
+        height: 14,
+        fontFamily: "Gotham Rounded",
+        fontSize: 15,
+        fontWeight: "500",
+        fontStyle: "normal",
+        lineHeight: 17,
+        letterSpacing: 0,
+        textAlign: "left"
+    },
+    
+    hospitalizationDescription : {
+        width: 277,
+        height: 12,
+        fontFamily: "Gotham Rounded",
+        fontSize: 13,
+        fontWeight: "normal",
+        fontStyle: "normal",
+        lineHeight: 14,
+        letterSpacing: 0,
+        textAlign: "left",
+        color: "#95989a",
+        marginTop: 10
+    }, 
+
+    lastVisit : {
+        width: 88,
+        height: 11,
+        fontFamily: "Gotham Rounded",
+        fontSize: 12,
+        fontWeight: "normal",
+        fontStyle: "normal",
+        lineHeight: 12,
+        letterSpacing: 0,
+        textAlign: "left",
+        color: "#51565f"
+    },
+
+    niceBlue: {color:  "#19769F"}, 
+
+    headerMenu: {backgroundColor: "#005cd1"}
+
+});
