ab11644ba4d6466c82922dcd33e071bcf7da84d7
jmjesus <jean.jesus@rededor.com.br>
2019-05-23 15:24:30 -0300

    ajuste no layout tela hospitais

--- src/pages/hospitals/style.js
+++ src/pages/hospitals/style.js
@@ -4,12 +4,11 @@ export default StyleSheet.create({
 	
 	container: {
     flexDirection: "row", 
-    justifyContent: "space-between", 
     alignItems: 'flex-start',
     backgroundColor: "#FFF",
     borderBottomWidth: 1,
     borderBottomColor: "#DDD",
-    padding: 0
+    width: '100%'
 	},
 
 	title: {
@@ -17,21 +16,18 @@ export default StyleSheet.create({
 		fontWeight: "bold",
     color: "#333", 
     padding: 5,
-    width: 300, 
+    width: '100%',
 	},
 	
 	hospitalIcon: {
-    width: 150,
-		height: 100,
-    resizeMode: 'center',
+    width: 175,
+    height: 86, 
   },
    
   niceBlue: {color:  "#19769F", paddingLeft: 2}, 
 
   description : {
-    width: 200,
-    maxWidth: 200, 
-    height: 15,
+    height: '50%',
     fontFamily: "Gotham Rounded",
     fontSize: 14,
     fontWeight: "normal",
@@ -41,20 +37,15 @@ export default StyleSheet.create({
     textAlign: "left",
     color: "#51565f" ,
     marginTop: 10, 
-    marginLeft: 10
+    marginLeft: 10, 
   }, 
 
   sideButtonRight : {
-    height: 110,
+    height: '100%',
     backgroundColor: "#005cd1",
-    marginLeft: 120,
-    paddingTop: 40, 
+    paddingTop: '8%', 
     fontWeight: "bold"
    }, 
-
-  justifyContent : {
-    justifyContent : 'space-between'
-  }, 
 
   headerMenu: {backgroundColor: "#005cd1"}, 
 
