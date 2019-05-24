5535fbaa7a3d634d3f72a7218b67c0f38b2077be
jmjesus <jean.jesus@rededor.com.br>
2019-05-24 12:51:42 -0300

    regras para calculo de pacientes e internados

--- src/pages/hospitals/index.js
+++ src/pages/hospitals/index.js
@@ -38,68 +38,80 @@ export default class Hospital extends Component {
 	}
 
 	getTotalPatientsVisited() {
-    this.state.hospitals.forEach(function(hospital) {
-			console.log(hospital.name)
-        //return countTotalPatientsVisited(hospital.patients)
-    });
+    	this.state.hospitals.forEach( hospital => {
+			hospital.totalPatientsVisitedToday = this.countTotalPatientsVisited(hospital.hospitalizationList)
+			hospital.totalPatients = this.countTotalPatients(hospital.hospitalizationList)
+			hospital.logomarca = this.getLogomarca(hospital)
+		});
+		console.log(this.state.hospitals)
 	}
 
-	getLogomarca(hospitals) {
-    hospitals.forEach(function(hospital) {
-        if(hospital.name.includes("copa dor")) {
+	getLogomarca(hospital) {
+        if(hospital.id === 61) {
+            return 'https://rededor.wpengine.com/wp-content/uploads/sites/27/2018/12/Logo-Real.svg'
+        } else if(hospital.id === 41) {
+            return 'https://wallpaperplay.com/walls/full/1/6/3/329998.jpg'
+        } else if(hospital.id === 21) {
+            return 'https://wallpaperplay.com/walls/full/1/6/3/329998.jpg'
+        } else if(hospital.id === 4) {
             return 'https://rededor.wpengine.com/wp-content/uploads/sites/15/2018/12/Logo-CopaDOr.svg'
-        } else if(hospital.name.includes("barra dor")) {
-            return 'https://rededor.wpengine.com/wp-content/uploads/sites/13/2018/12/Logo-BarraDOr.svg'
-        } else if(hospital.name.includes("niteroi")) {
-            return 'https://rededor.wpengine.com/wp-content/uploads/sites/24/2018/12/Logo-Niteroi.svg'
-        }
-        //Continua para os próximos hospitais
-    });
+        }  else if(hospital.id === 5) {
+            return 'http://www.copador.com.br/images/logo/niteroiDor_logo.png'
+        }  else if(hospital.id === 1) {
+            return 'https://wallpaperplay.com/walls/full/1/6/3/329998.jpg'
+        }  else if(hospital.id === 7) {
+            return 'http://www.copador.com.br/images/logo/oesteDor_logo.png'
+        }  else if(hospital.id === 2) {
+            return 'http://www.copador.com.br/images/logo/barraDor_logo.png'
+        }  else if(hospital.id === 9) {
+            return 'http://www.copador.com.br/images/logo/riosDor_logo.png'
+        }  else if(hospital.id === 101) {
+            return 'http://www.copador.com.br/images/logo/badim_logo.png'
+        }  else if(hospital.id === 6) {
+            return 'http://www.copador.com.br/images/logo/norteDor_logo.png'
+        }  else if(hospital.id === 3) {
+            return 'http://www.copador.com.br/images/logo/caxiasDor_logo.png'
+        }  else if(hospital.id === 8) {
+            return 'http://www.copador.com.br/images/logo/quintaDor_logo.png'
+        } 
 	}
 
-	countTotalPatientsVisited(patients) {
-    let totalPatientsVisited = patients.reduce((patientsVisited, patient) => {
-        let attendanceToday = hasAttendanceToday(patient)
-        if(attendanceToday) {
-            return patientsVisited + 1
-        } else {
-            return patientsVisited
-        }
-    }, 0);
-    return totalPatientsVisited;
+	countTotalPatientsVisited = patients => {
+		let totalPatientsVisited = patients.reduce((patientsVisited, patient) => {
+			
+			if(patient.exitDate === null) {
+				let attendanceToday = this.hasAttendanceToday(patient)
+				if(attendanceToday) {
+					return patientsVisited + 1
+				} else {
+					return patientsVisited
+				}
+			} else {
+				return patientsVisited;
+			}
+		}, 0);
+		return totalPatientsVisited;
 	}	
 
-	hasAttendanceToday(patient) {
-    const today = moment().format('YYYY-MM-DD');
-    let hasAttendance = patient.listaAttendance.find(visit => 
-        visit.startDate === today
-    );
-    return hasAttendance 
+	countTotalPatients = patients => {
+		let totalPatients = patients.reduce((totalPatients, patient) => {
+			
+			if(patient.exitDate === null) {
+				return totalPatients + 1;
+			} else {
+				return totalPatients;
+			}
+		}, 0);
+		return totalPatients;
 	}
 
-	calculateTotalPatientsInterned = () => {
-		const today = moment().format('YYYY-MM-DD')
-
-		let totalPatientsInterned = this.state.hospitals.map((hospital) => {
-			hospital.hospitalizationList.map((patient) => {
-				
-				patient.trackingList.map((visit) => { 
-					let visitDate = moment(visit.startDate).format('YYYY-MM-DD')
-					
-					console.log("visitDate", visitDate, " + " , visit)
-
-
-					console.log("today === visitDate", today === visitDate)
-
-					if(today === visitDate) {
-						console.log("MATCH")
-					}
-					//visit.startDate
-					//se tiver visita com data de hoje
-						//adiciona uma visita ao total de visitas do dia
-				})
-			})
-		})
+	hasAttendanceToday(patient) {
+		const today = moment().format('YYYY-MM-DD');
+		let hasAttendance = patient.trackingList.find(visit => {
+			let visitDate = moment(visit.startDate).format('YYYY-MM-DD')
+			return visitDate === today
+		});
+		return hasAttendance 
 	}
 	
 	loadHospitals = async (page = 1) => {
@@ -114,12 +126,10 @@ export default class Hospital extends Component {
 				  	"Token": userData.token,
 				}
 			}).then(response => {
-				console.log("response=> ", response)
 				if(response.status === 200) {
 					this.setState({
 						hospitals: [ ...this.state.hospitals, ...response.data.content.hospitalList], 
 					});
-					console.log(this.state)
 					this.getTotalPatientsVisited()
 					//this.calculateTotalPatientsInterned()
 					//chamar metodo que seta total de pacientes internados
@@ -151,7 +161,7 @@ export default class Hospital extends Component {
 			
 			<View style={[styles.container, {alignItems: 'center' }]}>
 				<View>
-					<Thumbnail square large source={{uri: item.image}} style={styles.hospitalIcon} />
+					<Thumbnail square large source={{uri: item.logomarca}} style={styles.hospitalIcon} />
 				</View>
 				<View style={{flexDirection: "column", width: '53%'}}>
 					<View>
@@ -163,12 +173,12 @@ export default class Hospital extends Component {
 
 					<View style={{flexDirection: "row", alignItems: 'center'}}>
 						<Icon type="AntDesign" name="calendar" style={styles.calendarIcon} />
-						<Text style={[styles.description]}>Internados: {item.hospitalizationList.length}</Text>
+						<Text style={[styles.description]}>Internados: {item.totalPatients}</Text>
 					</View>
 					
 					<View style={{flexDirection: "row", alignItems: 'center'}}>
 						<Icon type="AntDesign" name="user" style={styles.userIcon}/>
-						<Text style={[styles.description]}>Pendentes: {item.amount_patients}</Text>
+						<Text style={[styles.description]}>Pendentes: {item.totalPatientsVisitedToday}</Text>
 					</View>
 				</View>
 				<View style={[styles.sideButtonRight]}>
