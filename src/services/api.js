0c65ff07b8f3bc73df8b84bc45d208d83c73ecc4
Marcos Vinicius Pinto Fonseca <marcos.fonseca@rededor.com.br>
2019-05-28 17:55:35 -0300

    adicionando service, excluído sem intenção

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'http://10.25.35.84:8080/medico-consultor';
+
+axios.interceptors.request.use(request => {
+    console.log('Starting Request', request)
+    return request
+});
+  
+axios.interceptors.response.use(response => {
+    console.log('Response:', response)
+    return response
+});
+
+export default axios;
