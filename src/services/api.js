14a0cced52dfd098acd3383e02e07a15887ac55a
Marcos Vinicius Pinto Fonseca <marcos.fonseca@rededor.com.br>
2019-06-07 09:45:03 -0300

    voltando conf api service

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://appmedconsultor.rededor.com.br';
+axios.defaults.baseURL = 'http://10.25.35.84:8080/medico-consultor';
  
 axios.interceptors.request.use(request => {
     console.log('Starting Request', request)
