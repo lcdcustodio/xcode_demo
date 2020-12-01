ac38f374b80a8dc02a788972127dd01624c75b15
oberdan <oberdan@alis-sol.com.br>
2020-12-01 00:32:53 -0300

    update project

---
+++ ios/Pods/Target Support Files/Pods-medicoconsultor-tvOS/Pods-medicoconsultor-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_medicoconsultor_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_medicoconsultor_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-medicoconsultor/Pods-medicoconsultor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_medicoconsultor : NSObject
+@end
+@implementation PodsDummy_Pods_medicoconsultor
+@end
